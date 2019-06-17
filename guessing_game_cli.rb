955e57f81b7a1b7b739a0a247e21d893a5989c8f
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-17 03:07:05 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,18 +6,24 @@ def generate_number
 end
 
 def prompt_user
-  
+  puts "What is your guess?"
 end
 
 
 
 
 
-
-
-
-
-
 def run_guessing_game
-
+  the_number = generate_number
+  correct? = false
+  prompt_user
+  guess = gets.chomp
+  case guess
+    when guess == the_number
+      puts "You guessed the correct number!"
+    when guess == 'exit'
+      puts "Goodbye!"
+    else
+      puts "Sorry! The computer guessed #{the_number}"
+  end
 end
